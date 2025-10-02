977a9091eecec1dbe38aab8248727de37cc90502
yasarevalo <143626048+yasarevalo@users.noreply.github.com>
2025-10-02 08:24:16 -0300

    Login y Main

--- Flia.Chipana/conexion.php
+++ Flia.Chipana/conexion.php
@@ -1,18 +1,17 @@
 <?php
-class conexion{
-    static function conexionBD(){
+class Conexion {
+    public static function conexionBD() {   //cambio a clase estatica para no tener que intanciarla
+        $host = 'DESKTOP-C49M9HP\SQLEXPRESS';
+        $dbname = 'fliachipana';
+        $username = 'sa';
+        $password = 'Yasminarevalo2005';
+        $puerto = 1433;
 
-        $host='xx';
-        $dbname='xx';
-        $username='xx';
-        $pasword='xx';
-        $puerto= 1433;
-        
         try {
-            $conn = new PDO("sqlsrv:Server=$host,$puerto;Database=$dbname",$username,$pasword);
-            echo"conexión exitosa";
+            $conn = new PDO("sqlsrv:Server=$host,$puerto;Database=$dbname", $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //atrapar errores de sql de forma controlada
         } catch (PDOException $exp) {
-            echo ("Error a conectar la base de datos $dbname, error: $exp");
+            echo "Error a conectar la base de datos $dbname, error: " . $exp->getMessage();
         }
         return $conn;
     }
---
+++ Flia.Chipana/modulos/clientes.php
@@ -0,0 +1,61 @@
+<?php
+require_once("../conexion.php");
+
+// Conectar a la base de datos
+$conn = Conexion::conexionBD();
+
+// Consultar clientes
+$sql = "SELECT * FROM vw_mostrar_clientes";
+$stmt = $conn->query($sql);
+$clientes = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+
+
+    <!-- CONTENEDOR PRINCIPAL -->
+<div class="module-container-clientes">
+
+    <!-- HEADER -->
+    <div class="module-header-clientes">
+        <h2>CLIENTES</h2>
+        <div class="search-add-clientes">
+            <input type="text" placeholder="Buscar cliente" class="search-bar-clientes">
+            <button class="btn-add-clientes">Agregar items</button>
+        </div>
+    </div>
+
+    <!-- TABLA -->
+    <div class="table-scroll-container">
+
+    <table class="custom-table-clientes">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                <th>Teléfono</th>
+                <th>Dirección</th>
+                <th>Email</th>
+                <th>Deuda</th>
+                <th>Acción</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($clientes as $c): ?>
+            <tr>
+                <td><?= htmlspecialchars($c['id_cliente']) ?></td>
+                <td><?= htmlspecialchars($c['nombre']) ?></td>
+                <td><?= htmlspecialchars($c['apellido']) ?></td>
+                <td><?= htmlspecialchars($c['telefono']) ?></td>
+                <td><?= htmlspecialchars($c['direccion']) ?></td>
+                <td><?= htmlspecialchars($c['email'] ) ?></td>
+                <td><?= htmlspecialchars($c['deuda'] ) ?></td>
+                <td class="actions-clientes">
+                    <button class="btn-edit-clientes"><i class="fa-solid fa-pen-to-square"></i></button>
+                    <button class="btn-delete-clientes"><i class="fa-solid fa-trash-can"></i></button>
+                </td>
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+    </div>
