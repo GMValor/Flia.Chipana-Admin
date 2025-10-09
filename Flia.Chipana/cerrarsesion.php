fecb912d58fc6c1df42c8d538e7c12e56110feab
yasarevalo <143626048+yasarevalo@users.noreply.github.com>
2025-10-09 11:12:38 -0300

    eliminacion

--- Flia.Chipana/cerrarsesion.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();       // Iniciar la sesión
-session_unset();       // Eliminar todas las variables de sesión
-session_destroy();     // Destruir la sesión
-
-header("Location: index.php"); // Redirigir al login
-exit();
-?>
